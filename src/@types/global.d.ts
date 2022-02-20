cd3fe29c3faee31554027eebb3c99e58ddc76170
huaweiwei <harvey0379@163.com>
2022-02-21 00:08:41 +0800

    init

---
+++ src/@types/global.d.ts
@@ -0,0 +1,6 @@
+export {};
+
+declare global {
+  // eslint-disable-next-line
+  var mumu: object;
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import chemicalElement from './chemicalElement';
+import fruit from './fruit';
+
+const result = { chemicalElement, fruit };
+
+window.mumu = result;
+export default result;
